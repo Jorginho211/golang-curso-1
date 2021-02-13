2adef60fc857c4b008c02bc0779c954edbf44955
Jorge Lopez <jorgelopezseijas@gmail.com>
2021-02-13 11:32:14 +0100

    Ponteiros

---
+++ 8-Ponteiros/ponteiros.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Ponteiros")
+
+	var variavel1 int = 10
+	var variavel2 int = variavel1
+
+	fmt.Println(variavel1, variavel2)
+
+	variavel1++
+	fmt.Println(variavel1, variavel2)
+
+	// PONTEIRO E UMA REFERENCIA DE MEMORIA
+	var variavel3 int
+	var ponteiro *int //Endereço de memoria dum valor inteiro
+
+	variavel3 = 100
+	ponteiro = &variavel3
+	fmt.Println(variavel3, ponteiro)
+	fmt.Println(variavel3, *ponteiro) // desreferenciaçao
+
+	variavel3 = 150
+	fmt.Println(variavel3, ponteiro)
+	fmt.Println(variavel3, *ponteiro)
+}
