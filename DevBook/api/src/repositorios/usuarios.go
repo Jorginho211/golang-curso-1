95f9a342cc33dd1c5df9a5160244f8de64e41e25
Jorge Lopez <jorgelopezseijas@gmail.com>
2021-02-23 19:56:31 +0100

    [DevBook] Adicionase os metodos para poder parar de seguir um usuario dende a API

--- DevBook/api/src/repositorios/usuarios.go
+++ DevBook/api/src/repositorios/usuarios.go
@@ -159,3 +159,20 @@ func (repositorio usuarios) Seguir(usuarioId, seguidorId uint64) error {
 
 	return nil
 }
+
+// PararDeSeguir permite que um usuario pare de seguir o outro
+func (repositorio usuarios) PararDeSeguir(usuarioId, seguidorId uint64) error {
+	statement, erro := repositorio.db.Prepare(
+		"delete from seguidores where usuario_id = ? and seguidor_id = ?",
+	)
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(usuarioId, seguidorId); erro != nil {
+		return erro
+	}
+
+	return nil
+}
