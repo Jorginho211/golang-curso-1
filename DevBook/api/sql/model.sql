fcdbc421471924d44c9d941bd03af1fc6ff72451
Jorge Lopez <jorgelopezseijas@gmail.com>
2021-02-24 19:03:30 +0100

    [DevBook] Adicionase o modelo de Publicaçoes dentro em Go e SQL

--- DevBook/api/sql/model.sql
+++ DevBook/api/sql/model.sql
@@ -1,7 +1,10 @@
 CREATE DATABASE IF NOT EXISTS devbook;
 USE devbook;
 
+DROP TABLE IF EXISTS publicacoes;
+DROP TABLE IF EXISTS seguidores;
 DROP TABLE IF EXISTS usuarios;
+
 CREATE TABLE usuarios (
     id int auto_increment primary key,
     nome varchar(50) not null,
@@ -11,7 +14,6 @@ CREATE TABLE usuarios (
     criado timestamp default current_timestamp()
 ) ENGINE=INNODB;
 
-DROP TABLE IF EXISTS seguidores;
 CREATE TABLE seguidores (
     usuario_id int not null,
     FOREIGN KEY(usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE,
@@ -20,4 +22,16 @@ CREATE TABLE seguidores (
     FOREIGN KEY(seguidor_id) REFERENCES usuarios(id) ON DELETE CASCADE,
 
     PRIMARY KEY(usuario_id, seguidor_id)
+) ENGINE=INNODB;
+
+CREATE TABLE publicacoes (
+    id int auto_increment primary key,
+    titulo varchar(50) not null,
+    conteudo varchar(300) not null,
+
+    autor_id int not null,
+    FOREIGN KEY(autor_id) REFERENCES usuarios(id) ON DELETE CASCADE,
+
+    curtidas int default 0,
+    criadaEm timestamp default current_timestamp
 ) ENGINE=INNODB;
