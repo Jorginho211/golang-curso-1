a07b4e0473af4977c4f27e884b0717dd819f5f9e
Jorge Lopez <jorgelopezseijas@gmail.com>
2021-02-24 19:14:52 +0100

    [DevBook] Crianse as rotas e metodos no controller para as Publicaçoes

---
+++ DevBook/api/src/router/rotas/publicacoes.go
@@ -0,0 +1,39 @@
+package rotas
+
+import (
+	"api/src/controllers"
+	"net/http"
+)
+
+var rotasPublicacoes = []Rota{
+	{
+		URI:                 "/publicacoes",
+		Metodo:              http.MethodPost,
+		Funcao:              controllers.CriarPublicacao,
+		RequerAuthenticacao: true,
+	},
+	{
+		URI:                 "/publicacoes",
+		Metodo:              http.MethodGet,
+		Funcao:              controllers.BuscarPublicacoes,
+		RequerAuthenticacao: true,
+	},
+	{
+		URI:                 "/publicacoes",
+		Metodo:              http.MethodPost,
+		Funcao:              controllers.BuscarPublicacao,
+		RequerAuthenticacao: true,
+	},
+	{
+		URI:                 "/publicacoes/{publicacaoId}",
+		Metodo:              http.MethodPut,
+		Funcao:              controllers.AtualizarPublicacao,
+		RequerAuthenticacao: true,
+	},
+	{
+		URI:                 "/publicacoes",
+		Metodo:              http.MethodDelete,
+		Funcao:              controllers.DeletarPublicacao,
+		RequerAuthenticacao: true,
+	},
+}
