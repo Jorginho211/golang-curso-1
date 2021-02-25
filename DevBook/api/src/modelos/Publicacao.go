96ab563f4b860f2df77cd2557dcb7fd38bf16738
Jorge Lopez <jorgelopezseijas@gmail.com>
2021-02-25 18:56:32 +0100

    [DevBook] Adicionado o campo AutorNick no modelo de publicacao

--- DevBook/api/src/modelos/Publicacao.go
+++ DevBook/api/src/modelos/Publicacao.go
@@ -8,12 +8,13 @@ import (
 
 // Publicacao representa uma publicaçao feita por um usuario
 type Publicacao struct {
-	ID       uint64    `json:"id,omitempty"`
-	Titulo   string    `json:"titulo,omitempty"`
-	Conteudo string    `json:"conteudo,omitempty"`
-	AutorID  uint64    `json:"autorId,omitempty"`
-	Curtidas uint64    `json:"curtidas"`
-	CriadaEm time.Time `json:"criadaEm,omitempty"`
+	ID        uint64    `json:"id,omitempty"`
+	Titulo    string    `json:"titulo,omitempty"`
+	Conteudo  string    `json:"conteudo,omitempty"`
+	AutorID   uint64    `json:"autorId,omitempty"`
+	AutorNick string    `json:"autorNick,omitempty"`
+	Curtidas  uint64    `json:"curtidas"`
+	CriadaEm  time.Time `json:"criadaEm,omitempty"`
 }
 
 // Preparar vai chamar os metodos para validar e formatar a publicaçao recebida
