de902c44a33630e444847cbb3c315ccce50aa749
Jorge Lopez <jorgelopezseijas@gmail.com>
2021-02-25 18:31:00 +0100

    [DevBook] Adicionase a validaçao dos campos para criar a publicaçao

--- DevBook/api/src/modelos/Publicacao.go
+++ DevBook/api/src/modelos/Publicacao.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Publicacao representa uma publicaçao feita por um usuario
 type Publicacao struct {
@@ -10,4 +14,31 @@ type Publicacao struct {
 	AutorID  uint64    `json:"autorId,omitempty"`
 	Curtidas uint64    `json:"curtidas"`
 	CriadaEm time.Time `json:"criadaEm,omitempty"`
+}
+
+// Preparar vai chamar os metodos para validar e formatar a publicaçao recebida
+func (publicacao *Publicacao) Preparar() error {
+	if erro := publicacao.validar(); erro != nil {
+		return erro
+	}
+
+	publicacao.formatar()
+	return nil
+}
+
+func (publicacao *Publicacao) validar() error {
+	if publicacao.Titulo == "" {
+		return errors.New("O titulo é obrigatorio e nao pode estar em branco!")
+	}
+
+	if publicacao.Conteudo == "" {
+		return errors.New("O titulo é obrigatorio e nao pode estar em branco!")
+	}
+
+	return nil
+}
+
+func (publicacao *Publicacao) formatar() {
+	publicacao.Titulo = strings.TrimSpace(publicacao.Titulo)
+	publicacao.Conteudo = strings.TrimSpace(publicacao.Conteudo)
 }
